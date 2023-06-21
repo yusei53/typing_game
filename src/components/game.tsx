ada6843882cdb39f0089bb09dfbb092e8ee9c46c
今村悠聖 <yusei@imamurayuushounoMacBook-Air.local>
2023-06-21 11:40:25 +0900

    スコア、残り時間、タイトル表示

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -1,6 +1,18 @@
-export const Game = () => {
-    return (
-        <div className="flex justify-center items-center h-screen bg-custom-background">
-        </div>
-    )
+import { useState } from "react";
+
+export const Game = (props: { title: string }) => {
+  const title = props.title;
+
+  const [score, setScore] = useState(0);
+  const [timeLeft, setTimeLeft] = useState(60);
+
+  return (
+    <div className="flex justify-center items-center h-screen bg-custom-background">
+      <div className="h-4/5 p-8 font-bold text-white justify-center text-center">
+        <p className="text-4xl mb-6">{title}</p>
+        <p className="text-3xl mb-2">スコア：{score}</p>
+        <p className="text-3xl">残り時間：{timeLeft}</p>
+      </div>
+    </div>
+  );
 };
